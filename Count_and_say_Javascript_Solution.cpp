a3ae455e94bc8d53dc816ecfd214ac5310fa736f
yogeshsakle <yogeshsakle8@gmail.com>
2022-06-18 00:31:05 +0530

    Done

---
+++ Count_and_say_Javascript_Solution.cpp
@@ -0,0 +1,26 @@
+/** JavaScript Solution**/
+
+var countAndSay = function(n) {
+         let ans = '1';
+         for(let i=0; i<n-1 ; i++){
+             ans=say(ans);
+         }
+         return ans;
+};
+var say = function(temp){
+    let count=0;
+    let curr=temp[0];
+    let str="";
+    
+    for (let i=0; i<=temp.length;i++){
+        if(curr==temp[i]){
+            count++;
+        }else{
+            str +=count+curr;
+            count=1;
+            curr=temp[i]
+        }   
+    }
+return str;    
+}
+
---
+++ Implement_strStr()_Javascript_Solution.cpp
@@ -0,0 +1,20 @@
+/** JavaScript Solution**/
+var strStr = function(haystack, needle) {
+   
+    if (needle === '' || needle === haystack) return 0; 
+    var left = 0;
+    var right = needle.length;
+
+    while(right <= haystack.length) {
+        if(haystack.slice(left, right) == needle) {
+            return left;
+        } 
+        else{
+             left++;
+             right++;
+         }
+     }
+    
+    return -1;
+};
+
